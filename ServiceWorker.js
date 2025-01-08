5aa916ce2ae958a122df14ed60a476be695d1899
Valentina-sketch <valentinallns1014@gmail.com>
2025-01-08 11:09:29 -0700

    Added build files via upload
    Uploaded WebGL Unity Build Version 0.1

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "ValentinaLl-RocketCar-0.1";
+const contentToCache = [
+    "Build/RocketCar_Web_BuildVL.loader.js",
+    "Build/RocketCar_Web_BuildVL.framework.js.unityweb",
+    "Build/RocketCar_Web_BuildVL.data.unityweb",
+    "Build/RocketCar_Web_BuildVL.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
